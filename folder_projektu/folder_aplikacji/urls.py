72822990d2d2f3f1026edddbf982f4e5b904e89d
Twoje <zwarchala@gmail.com>
2024-12-04 16:17:30 +0100

    lab_07

---
+++ folder_projektu/folder_aplikacji/urls.py
@@ -0,0 +1,9 @@
+# plik ankiety/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('persons/', views.person_list),
+    path('persons/<int:pk>/', views.person_detail),
+]
